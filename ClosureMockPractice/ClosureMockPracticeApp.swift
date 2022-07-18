4da53c2daa0632b7b87322feacd80705ae65b267
Kotaro Fukuo <haptaro.dev@gmail.com>
2022-07-17 17:04:15 -0700

    Initial Commit

---
+++ ClosureMockPractice/ClosureMockPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosureMockPracticeApp.swift
+//  ClosureMockPractice
+//
+//  Created by Kotaro Fukuo on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct ClosureMockPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosureMockPractice/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ClosureMockPractice
+//
+//  Created by Kotaro Fukuo on 2022/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
